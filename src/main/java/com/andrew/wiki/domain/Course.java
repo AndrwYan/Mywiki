3d99a7e6d6246f2e8a8b640042c48661a60708e6
AndrwYan <beyond1997110@gmail.com>
2021-11-06 13:08:05 +0800

    Mybatis自动生成持久层代码

---
+++ src/main/java/com/andrew/wiki/domain/Course.java
@@ -0,0 +1,46 @@
+package com.andrew.wiki.domain;
+
+public class Course {
+    private String cid;
+
+    private String cname;
+
+    private String tid;
+
+    public String getCid() {
+        return cid;
+    }
+
+    public void setCid(String cid) {
+        this.cid = cid;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    public String getTid() {
+        return tid;
+    }
+
+    public void setTid(String tid) {
+        this.tid = tid;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", cid=").append(cid);
+        sb.append(", cname=").append(cname);
+        sb.append(", tid=").append(tid);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/andrew/wiki/mapper/CourseMapper.java
@@ -0,0 +1,22 @@
+package com.andrew.wiki.mapper;
+
+import com.andrew.wiki.domain.Course;
+import com.andrew.wiki.domain.CourseExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CourseMapper {
+    long countByExample(CourseExample example);
+
+    int deleteByExample(CourseExample example);
+
+    int insert(Course record);
+
+    int insertSelective(Course record);
+
+    List<Course> selectByExample(CourseExample example);
+
+    int updateByExampleSelective(@Param("record") Course record, @Param("example") CourseExample example);
+
+    int updateByExample(@Param("record") Course record, @Param("example") CourseExample example);
+}
